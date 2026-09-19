8a3a1bc17d46ace277828871a3ab25028db2461f
Fikri Mulya Rachmat <frahmat68@gmail.com>
2026-09-20 00:25:11 +0700

    feat(executor): wire deadman switch to order router and main loop

--- KiBot V2/executor/__init__.py
+++ KiBot V2/executor/__init__.py
@@ -1,4 +1,14 @@
 from .virtual_ledger import VirtualLedger, VirtualPosition
 from .order_router import OrderRouter
+from .deadman import IndodaxDeadmanSwitch, deadman_switch, register_deadman, heartbeat, cancel_all_if_dead
 
-__all__ = ["VirtualLedger", "VirtualPosition", "OrderRouter"]
+__all__ = [
+    "VirtualLedger",
+    "VirtualPosition",
+    "OrderRouter",
+    "IndodaxDeadmanSwitch",
+    "deadman_switch",
+    "register_deadman",
+    "heartbeat",
+    "cancel_all_if_dead",
+]
